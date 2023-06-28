1127578e110b086895e6169ab00907c879cbd4a2
abdolm <sasanabdolm@gmail.com>
2023-06-28 21:33:15 +0200

    envoie

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -16,5 +16,11 @@ namespace Bulky.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
     }
 }
